7dd015e602283897b3b848d2f23b2428fca265ba
hx <hengxuan.dong@gmail.com>
2021-05-09 12:42:55 -0700

    login function done first version.

--- src/main/java/com/example/bank266/DemoApplication.java
+++ src/main/java/com/example/bank266/DemoApplication.java
@@ -31,20 +31,4 @@ public class DemoApplication implements CommandLineRunner {
 			System.out.println("bank266 password: " + userInfo.getPassword());
 		}
 	}
-
-	@GetMapping("/hello")
-	public String hello(@RequestParam(value = "name", defaultValue = "Nina") String name) {
-		return String.format("Hello %s!", name);
-	}
-
-	@RequestMapping("/")
-	public String index() {
-		return "login";
-	}
-
-	@PostMapping("/login")
-	public void login(@RequestParam(value = "username", defaultValue = "") String username,
-						@RequestParam(value = "password", defaultValue = "") String password) {
-		System.out.println(username + " : " + password);
-	}
 }
